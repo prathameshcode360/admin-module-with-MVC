8f89a52121010ab4a332e2d3c1883526a240b02e
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-07 13:51:40 +0530

    added express validator validation

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,20 +1,25 @@
-export default function validation(req, res, next) {
-  const { name, price, image } = req.body;
-  let errors = [];
-  if (!name || name.trim() == "") {
-    errors.push("Invalid Name");
-  }
-  if (!price || parseFloat(price) < 1) {
-    errors.push("Price should positive number");
-  }
-  try {
-    let validUrl = new URL(image);
-  } catch (err) {
-    errors.push("Invalid URL");
+import { body, validationResult } from "express-validator";
+
+export default async function validation(req, res, next) {
+  // 1. Setting rules
+  const rules = [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be a positive number"),
+    body("image").isURL().withMessage("Invalid URL"),
+  ];
+
+  // 2. Run the validation rules
+  await Promise.all(rules.map((rule) => rule.run(req)));
+
+  // 3. Check for validation errors
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return res.render("addProduct", {
+      errorMessage: validationErrors.array()[0].msg, // Send only the error message
+    });
   }
 
-  if (errors.length > 0) {
-    return res.render("addProduct", { errorMessage: errors[0] });
-  }
-  next();
+  next(); // Proceed to the next middleware or route handler
 }
