377b1acce5eb57d02e01523b1bf60793f51bfcc0
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-07 12:58:38 +0530

    validation added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -8,10 +8,27 @@ export default class ProductController {
   }
 
   renderAddProduct(req, res) {
-    res.render("addProduct");
+    res.render("addProduct", { errorMessage: null });
   }
   addNewProduct(req, res) {
-    // console.log(req.body);
+    const { name, price, image } = req.body;
+    let errors = [];
+    if (!name || name.trim() == "") {
+      errors.push("Invalid Name");
+    }
+    if (!price || parseFloat(price) < 1) {
+      errors.push("Price should positive number");
+    }
+    try {
+      let validUrl = new URL(image);
+    } catch (err) {
+      errors.push("Invalid URL");
+    }
+
+    if (errors.length > 0) {
+      return res.render("addProduct", { errorMessage: errors[0] });
+    }
+
     ProductModel.add(req.body);
     let products = ProductModel.getAll();
     res.render("index", { products: products });
