f2e5777b0c198585bf4397833f3a4c5ffd81b8bf
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-07 19:24:57 +0530

    added all functionality

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -37,21 +37,21 @@ var products = [
     1,
     "Product 1",
     "Description for Product 1",
-    19.99,
+    200,
     "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg"
   ),
   new ProductModel(
     2,
     "Product 2",
     "Description for Product 2",
-    29.99,
+    100,
     "https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg"
   ),
   new ProductModel(
     3,
     "Product 3",
     "Description for Product 3",
-    39.99,
+    250,
     "https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg"
   ),
 ];
